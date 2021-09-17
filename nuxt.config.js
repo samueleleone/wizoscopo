5a6c63a5a9a8d7e956c28b2d5a74b448c8e450b3
Samuele Leone <44635579+wiztoth@users.noreply.github.com>
2021-09-17 18:07:24 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,9 @@ export default {
     htmlAttrs: {
       lang: 'it'
     },
+     generate: {
+    dir: 'public'
+  },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
