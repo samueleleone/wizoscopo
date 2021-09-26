fac39449e282371efea8b5a2ba47b3a11d138554
Samuele Leone <samueleartisanleone@gmail.com>
2021-09-26 13:03:32 +0200

    new fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,9 +30,7 @@ export default {
   css: [
   ],
 
-  serverMiddleware: [
-    '~/api/index.js'
- ],
+
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
