3f372da5d8e2a52f0d5ba3986fc041c57f68fe22
Samuele Leone <samueleartisanleone@gmail.com>
2021-09-26 12:34:33 +0200

    new api interface

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,10 @@ export default {
   css: [
   ],
 
+  serverMiddleware: [
+    '~/api/index.js'
+ ],
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
 
